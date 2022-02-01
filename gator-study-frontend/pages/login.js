7f83eb1f3bb6461045fbe6ec04d2a83ff7b24b52
abhiti01 <abhiti1199@gmail.com>
2022-01-31 20:42:27 -0500

    add basic login page

---
+++ gator-study-frontend/pages/login.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import Router from "next/router";
+// import Layout from "../component/Layout";
+// import { login } from "../requests/userApi";
+// import useUser from "../data/useUser";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+//   const { mutate, loggedIn } = useUser();
+
+//   useEffect(() => {
+//     if (loggedIn) Router.replace("/");
+//   }, [loggedIn]);
+
+//   if (loggedIn) return <> Redirecting.... </>;
+
+  const onLoginSubmit = (e) => {
+    e.preventDefault();
+    if (email && password) {
+    //   login({ email, password });
+    console.log(email,password);
+    //   mutate();
+    }
+  };
+
+  return (
+    // <Layout title="login">
+      <div >
+        <h2 > login </h2>
+
+        <form onSubmit={onLoginSubmit}>
+          <div >
+            <label htmlFor="email">Email address</label>
+            <input
+              value={email}
+              type="email"
+              className="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="form-control"
+              id="password"
+            />
+          </div>
+
+          <button type="submit" >
+            Submit
+          </button>
+        </form>
+      </div>
+    // </Layout>
+  );
+};
+
+export default Login;
