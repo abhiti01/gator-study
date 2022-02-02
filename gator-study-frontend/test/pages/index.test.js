19cee70924dcb3f5c8101527bffbcbd5f0e0eb39
abhiti01 <abhiti1199@gmail.com>
2022-02-01 19:31:57 -0500

    add unit tests for timer

--- gator-study-frontend/test/pages/index.test.js
+++ gator-study-frontend/test/pages/index.test.js
@@ -1,25 +1,52 @@
 import React from "react";
-// Using render and screen from test-utils.js instead of
-// @testing-library/react
 import { render, screen } from "../test-utils";
 import Home from "../../pages/index";
+import { isResSent } from "next/dist/shared/lib/utils";
+import { MdOutlineAlarm } from "react-icons/md";
 describe("Home", () => {
   it("Renders home page", () => {
     render(<Home />);
   
-
-    // we can only use toBeInTheDocument because it was imported
-    // in the jest.setup.js and configured in jest.config.js
-    
   });
-describe("check timer",() => {
+
+});
+describe("Check timer",() => {
   it("sets stage correctly",() =>{
     try {
-      expect(switchState(0)[0]).toBe(0);
+      expect(switchState(0)).toBe('pomodoro');
+      expect(switchState(1)).toBe('short break');
+      expect(switchState(2)).toBe('long break');
       done();
     } catch (error) {
-      print(error);
     }
   });
-});
+  it("timer start correctly and sets approprite flags",() =>{
+    try {
+      startTimer();
+      expect(isTicking.toBe(true));
+      expect(isTimeUp.toBe(false));
+      done();
+    } catch (error) {
+    }
+  });
+  it("on time up, resets everything",() =>{
+    try {
+      reset();
+      expect(isTicking.toBe(false));
+      expect(isTimeUp.toBe(true));
+      expect(consumedSecond.toBe(false));
+      done();
+    } catch (error) {
+    }
+  });
+  it("mute alarm button works as expected",() =>{
+    try {
+      muteAlarm();
+      expect(alarmRef.current.pause().toBe(true));
+      expect(alarmRef.current.time.toBe(0));
+      expect(consumedSecond.toBe(false));
+      done();
+    } catch (error) {
+    }
+  });
 });
