d7bb0397ea835db24256cf695193d517ee7a3e57
Naman Bhatia <naman.universe@gmail.com>
2022-02-04 16:05:28 -0500

    Added register page and api calls

--- gator-study-frontend/pages/index.js
+++ gator-study-frontend/pages/index.js
@@ -3,10 +3,13 @@ import Navigations from "../components/Navigations";
 import Home from "./time";
 import Simple from "../components/newNavBar";
 import Login from "./login";
+import Register from "./resgister";
 export default function Chats() {
   const [userLogin, setUserLogin] = useState(false);
+  const [userRegister, setUserRegister] = useState(false);
   return <div className="background">
     <Simple setUserLogin = {setUserLogin}/>
+    <Simple setUserRegister = {setUserRegister}/>
     <Home />
      </div>;
 } 
