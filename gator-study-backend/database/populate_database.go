dc7e0ca6be24f4e67a756d0371f0fd74068b4167
yagyamalik <yagyam23@gmail.com>
2022-04-20 16:43:32 -0400

    Revert "Merge branch 'main' of https://github.com/abhiti01/gator-study"
    This reverts commit 046e2a2ea376a6d8951ce605b1851346ec95f5c4, reversing
    changes made to 61305794ba82d9f3f7769a79a9e2ef0793c12428.

--- gator-study-backend/database/populate_database.go
+++ gator-study-backend/database/populate_database.go
@@ -8,28 +8,28 @@ import (
 )
 
 func PopulateGroups() {
-	const GroupCount int = 4
-	var Id = [GroupCount]int{0, 1, 2, 3}
-	var Name = [GroupCount]string{"Business", "Architecture", "Computer Science", "Arts"}
-	var Moderator = [GroupCount]string{"Akshat", "Naman", "Abhiti", "Yagya"}
-	var capacity = [GroupCount]int{30, 30, 30, 30}
-	var descriptions = [GroupCount]string{
+	const teachercount int = 4
+	var Id = [teachercount]int{0, 1, 2, 3}
+	var Name = [teachercount]string{"Business", "Architecture", "Computer Science", "Arts"}
+	var Moderator = [teachercount]string{"Akshat", "Naman", "Abhiti", "Yagya"}
+	var capacity = [teachercount]int{30, 30, 30, 30}
+	var descriptions = [teachercount]string{
 		"The main aim of this study group is to create an atmosphere where students can learn about Business. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
 		"The main aim of this study group is to create an atmosphere where students can learn about Architecture. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
 		"The main aim of this study group is to create an atmosphere where students can learn about Computer Science. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
 		"The main aim of this study group is to create an atmosphere where students can learn about Arts. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
 	}
-	var imagepath = [GroupCount]string{
+	var imagepath = [teachercount]string{
 		"img/group/Business.jpg",
 		"img/group/Architecture.jpg",
 		"img/group/Computer Science.jpg",
 		"img/group/Arts.jpg",
 	}
 
-	var Group []models.Group
+	var teachers []models.Teacher
 
-	for i := 0; i < GroupCount; i++ {
-		Group = append(Group, models.Group{
+	for i := 0; i < teachercount; i++ {
+		teachers = append(teachers, models.Teacher{
 			Id:          Id[i],
 			Name:        names[i],
 			Description: descriptions[i],
@@ -38,7 +38,7 @@ func PopulateGroups() {
 			ImagePath:   imagepath[i],
 		})
 	}
-	DB.Create(&Group)
+	DB.Create(&teachers)
 	log.Println(result.Error, result.RowsAffected)
 }
 
