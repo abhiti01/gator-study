acab70b869bba54c7fc0befeb7a9c536148a2140
Naman Bhatia <naman.universe@gmail.com>
2022-04-20 20:39:31 -0400

    Code refactoring

--- gator-study-backend/database/populate_database.go
+++ gator-study-backend/database/populate_database.go
@@ -8,43 +8,42 @@ import (
 )
 
 func PopulateGroups() {
-	const teachercount int = 4
-	var Id = [teachercount]int{0, 1, 2, 3}
-	var Name = [teachercount]string{"Business", "Architecture", "Computer Science", "Arts"}
-	var Moderator = [teachercount]string{"Akshat", "Naman", "Abhiti", "Yagya"}
-	var capacity = [teachercount]int{30, 30, 30, 30}
-	var descriptions = [teachercount]string{
-		"The main aim of this study group is to create an atmosphere where students can learn about Business. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
-		"The main aim of this study group is to create an atmosphere where students can learn about Architecture. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
-		"The main aim of this study group is to create an atmosphere where students can learn about Computer Science. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
-		"The main aim of this study group is to create an atmosphere where students can learn about Arts. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
+	const groupcount int = 4
+	var Id = [groupcount]int{0, 1, 2, 3}
+	var names = [groupcount]string{"AOA", "DOSP", "Java", "Python"}
+	var Moderator = [groupcount]string{"Akshat", "Naman", "Abhiti", "Yagya"}
+	var Capacity = [groupcount]int{30, 30, 30, 30}
+	var descriptions = [groupcount]string{
+		"The main aim of this study group is to create an atmosphere where students can learn about AOA. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
+		"The main aim of this study group is to create an atmosphere where students can learn about DOSP. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
+		"The main aim of this study group is to create an atmosphere where students can learn about Java. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
+		"The main aim of this study group is to create an atmosphere where students can learn about Python. Students can use chat to ask doubts. No tolerance on sexual or racist comments.",
 	}
-	var imagepath = [teachercount]string{
-		"img/group/Business.jpg",
-		"img/group/Architecture.jpg",
-		"img/group/Computer Science.jpg",
-		"img/group/Arts.jpg",
+	var imagepath = [groupcount]string{
+		"icons/AOA.png",
+		"icons/DOSP.png",
+		"icons/Java.png",
+		"icons/Python.png",
 	}
 
-	var teachers []models.Teacher
+	var groups []models.Group
 
-	for i := 0; i < teachercount; i++ {
-		teachers = append(teachers, models.Teacher{
-			Id:          Id[i],
+	for i := 0; i < groupcount; i++ {
+		groups = append(groups, models.Group{
+			Id:          strconv.Itoa(Id[i]),
 			Name:        names[i],
 			Description: descriptions[i],
 			Moderator:   Moderator[i],
-			Capacity:    Capacity[i],
+			Capacity:    strconv.Itoa(Capacity[i]),
 			ImagePath:   imagepath[i],
 		})
 	}
-	DB.Create(&teachers)
+	result := DB.Create(&groups)
 	log.Println(result.Error, result.RowsAffected)
 }
 
 func populateUsers() {
 	const usercount int = 10
-	var Id = [usercount]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	var names = [usercount]string{"Akshat", "Naman", "Abhiti", "Yagya", "Alex", "Brady", "Charlie", "Daanish", "Ely", "Furgason"}
 
 	var emails = [usercount]string{"Akshat@gmail.com", "Naman@gmail.com", "Abhiti@gmail.com", "Yagya@gmail.com", "Alex@gmail.com", "Brady@gmail.com", "Charlie@gmail.com", "Daanish@gmail.com", "Ely@gmail.com", "Furgason@gmail.com"}
@@ -53,10 +52,10 @@ func populateUsers() {
 
 	for i := 0; i < usercount; i++ {
 		password := []byte(default_password + "" + strconv.Itoa(i+1))
-		hash, _ := bcrypt.GenerateFromPassword(password, config.PasswordCost)
+		hash, _ := bcrypt.GenerateFromPassword(password, 10)
 
 		users = append(users, models.User{
-			Id:       uint(Id[i]),
+			Name:     names[i],
 			Email:    emails[i],
 			Password: hash,
 		})
